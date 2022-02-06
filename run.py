b0441137d4138bf59af90e5cedbeaefcce04be20
smithylee81 <smithylee81@yahoo.co.uk>
2022-02-06 19:50:47 +0000

    Add advance routing and POST method

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -12,12 +13,27 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
